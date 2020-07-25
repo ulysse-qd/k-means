e4d463cc367c4ade622c00c3e1ad49c978db23ba
Ulysse Queritet-Diop <le-ul@hotmail.fr>
2020-07-25 17:23:54 +0200

    [Update] add possibility to choose number of dimensions for every points.

--- k_means_fcn.py
+++ k_means_fcn.py
@@ -15,60 +15,63 @@ def usage():
     if len(sys.argv) == 2:
         if sys.argv[1] == "-h":
             print("USAGE")
-            print("    ./k_means_fcn.py pt cl")
+            print("    ./k_means_fcn.py pt n cl")
             print("DESCRIPTION")
             print("    pt      Number of points")
+            print("    n       Number of dimensions")
             print("    cl      Number of clusters")
             exit(0)
     return
 
 def check_error():
-    if len(sys.argv) > 3 or len(sys.argv) == 1:
+    if len(sys.argv) > 4 or len(sys.argv) == 1:
         print("Wrong number of arguments, try \"-h\" for more informations")
         exit(84)
-    if sys.argv[1].isdigit() and sys.argv[2].isdigit():
-        return
-    else:
+    if sys.argv[1].isdigit() != True or sys.argv[2].isdigit() != True or sys.argv[3].isdigit() != True:
         print("Arguments have to be numerical characters.")
         exit(84)
+    if int(sys.argv[1]) <= 0 or int(sys.argv[2]) <= 0 or int(sys.argv[3]) <= 0:
+        print("Arguments can't be 0 or less.")
+        exit(84)
+    return
 
-def points_and_barycenter_generator(pts, cl):
-    pts_xyd = np.zeros((pts, 2), dtype=int)
-    bcenter = np.zeros((cl, 2), dtype=int)
+def points_and_barycenter_generator(pts, n, cl):
+    pts_xyd = np.zeros((pts, n), dtype=int)
+    bcenter = np.zeros((cl, n), dtype=int)
     max_coord = pts * 2
 
     for i in range(len(bcenter)):
-        for j in range(2):
+        for j in range(n):
             bcenter[i][j] = np.random.randint(0, max_coord)
     for i in range(len(pts_xyd)):
-        for j in range(2):
+        for j in range(n):
             pts_xyd[i][j] = np.random.randint(0, max_coord)
+    print("First bary :\n", bcenter, "\n\n")
     return pts_xyd, bcenter
 
-def k_means_algorithm(points, bary):
+def k_means_algorithm(points, bary, dim):
     norm = np.zeros((len(points), len(bary)), dtype=int)
 
-    print("First bary :\n", bary, "\n\n")
     for i in range(len(points)):
         for j in range(len(bary)):
             norm[i][j] = np.linalg.norm(points[i] - bary[j], axis=0)
     smallest = np.argmin(norm, axis=1)
     for i in range(len(bary)):
-        mean = np.empty((0, 2), dtype=int)
+        mean = np.empty((0, dim), dtype=int)
         for j in range(len(smallest)):
             if i == smallest[j]:
                 mean = np.append(mean, [points[j]], axis=0)
         if len(mean) != 0:
             bary[i] = np.mean(mean, axis=0)
-    print("new bary :\n", bary)
     return bary
 
 if __name__ == '__main__':
     usage()
     check_error()
     points = int(sys.argv[1])
-    clusters = int(sys.argv[2])
-    points_and_barycenter_generator(points, clusters)
-    points_coord, bary_coord = points_and_barycenter_generator(points, clusters)
+    dim = int(sys.argv[2])
+    clusters = int(sys.argv[3])
+    points_coord, bary_coord = points_and_barycenter_generator(points, dim, clusters)
     for i in range(100):
-        bary_coord = k_means_algorithm(points_coord, bary_coord)
+        bary_coord = k_means_algorithm(points_coord, bary_coord, dim)
+    print("New barycenters :\n", bary_coord)
